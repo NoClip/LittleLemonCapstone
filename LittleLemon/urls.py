dd9c9be5e11eedeefc4d65c60c7e5e207ee9206b
QWERTY <bassam.mh@gmail.com>
2023-07-04 18:44:47 +0300

    menu items

--- LittleLemon/urls.py
+++ LittleLemon/urls.py
@@ -1,36 +1,15 @@
-"""LittleLemon URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-# from LittleLemonAPI import views
 from restaurant import views
-""" router = routers.DefaultRouter()
-router.register(r'menuitems', views.menuViewSet) """
+from rest_framework.routers import DefaultRouter
 
-""" urlpatterns = [
-    path('api/', include(router.urls)),
-    path('admin/', admin.site.urls),
-] """
-""" urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls'))
-] """
+router = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurant/', include('restaurant.urls')),
+    path('restaurant/menu/', include('restaurant.urls')),
+    path('restaurant/booking/', include(router.urls)),
 ]
